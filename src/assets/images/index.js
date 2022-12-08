7b71c060f24bcc4b61515dea46550800d56c659e
zafiragalea <87572241+zafiragalea@users.noreply.github.com>
2022-12-08 15:01:36 +0700

    newprofile UI

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,5 +3,6 @@ import ilustration from "./Ilustration.png";
 import photoProfile from "./photoProfile.svg"
 import backIcon from "./backIcon.svg"
 import flashIcon from "./flashIcon.svg"
+import barcodeIcon from "./barcodeIcon.svg"
 
-export {logo, ilustration, photoProfile, backIcon, flashIcon};
+export {logo, ilustration, photoProfile, backIcon, flashIcon, barcodeIcon};
--- src/pages/mahasiswa/profile/Profile.jsx
+++ src/pages/mahasiswa/profile/Profile.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import { ProfileWrapper } from './Profile.style.js'
-import { photoProfile } from "../../../assets/images";
+import { photoProfile, barcodeIcon } from "../../../assets/images";
 
 const Profile = () => (
     <ProfileWrapper>
         <div className="header-sementara">
-            <h1>History Absensi</h1>
+            <h1>--</h1>
         </ div>
         <div className="profile-absen">
             <img alt="PhotoProfile" className="photo-profile" src={photoProfile} />
@@ -14,21 +14,76 @@ const Profile = () => (
                 <p>Selamat datang, Shabrina Rafika!</p>
                 <p>09021281924049</p>
                 <div className="absenQR-section">
-                    <button className="absen-QR" type="button">
+                    <div className="absen-QR">
                         <Link to="/scanner">Absen dulu, yuk!</Link>
-                    </button>
+                        <img alt="barcodeIcon" className="barcodeIcon" src={barcodeIcon} />
+                    </div>
                 </div>
             </div>
         </ div>
-        <div className="history">
-            <div className="history-recap">
-                <p>29 - 09 - 2022</p>
-                <p>Masuk : 10.00.01</p>
+        <div className="history-peminjaman">
+            <div className="title">
+                <p>Peminjaman</p>
+            </div>
+            <div className="riwayat-peminjaman">
+                <img alt="PhotoProfile" className="book-photo" src={photoProfile} />
+                <table className="riwayat-recap">
+                    <tr>
+                        <td>Judul</td>
+                        <td>:</td>
+                        <td>xxx</td>
+                    </tr>
+                    <tr>
+                        <td>Status</td>
+                        <td>:</td>
+                        <td>xxx</td>
+                    </tr>
+                    <tr>
+                        <td>Tenggat Waktu</td>
+                        <td>:</td>
+                        <td>Senin, 21 November 2022 </td>
+                    </tr>
+                </table>
+            </div>
+            <div className="riwayat-peminjaman">
+                <img alt="PhotoProfile" className="book-photo" src={photoProfile} />
+                <table className="riwayat-recap">
+                    <tr>
+                        <td>Judul</td>
+                        <td>:</td>
+                        <td>xxx</td>
+                    </tr>
+                    <tr>
+                        <td>Status</td>
+                        <td>:</td>
+                        <td>xxx</td>
+                    </tr>
+                    <tr>
+                        <td>Tenggat Waktu</td>
+                        <td>:</td>
+                        <td>Senin, 21 November 2022 </td>
+                    </tr>
+                </table>
+            </div>
+            <button className="riwayat-button" type="button">
+                <Link to="/profile">Riwayat Peminjaman</Link>
+            </button>
+        </div>
+        <div className="history-absen">
+            <div className="title">
+                <p>Absen</p>
             </div>
             <div className="history-recap">
                 <p>29 - 09 - 2022</p>
                 <p>Masuk : 10.00.01</p>
             </div>
+            <div className="history-recap">
+                <p>29 - 09 - 2022</p>
+                <p>Masuk : 10.00.01</p>
+            </div>
+            <button className="riwayat-button" type="button">
+                <Link to="/profile">Riwayat Absen</Link>
+            </button>
         </div>
         <div className="Footer">
         </div>
