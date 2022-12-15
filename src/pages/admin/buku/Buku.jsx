9c23ae318dfd0d5b3bb6f3d40686b60e7c4a0b88
tariraf <tarisarafika2@gmail.com>
2022-12-15 10:34:24 +0700

    create searchbar for katalog admin pages

--- src/pages/admin/buku/Buku.jsx
+++ src/pages/admin/buku/Buku.jsx
@@ -2,22 +2,35 @@ import React from 'react';
 import CardCatalog from '../../../components/elements/card_catalog/CardCatalog';
 import BukuWrapper from './Buku.style';
 import { DataBuku } from './BukuData';
+import * as BiIcons from 'react-icons/bi'
 const Buku = () => {
     console.log(DataBuku)
     return (
         <BukuWrapper>
-            {
-                DataBuku.map((item,index)=>{
-                    return (
-                        <CardCatalog 
-                            key={index}
-                            label={item.label}
-                            bookImg={item.bookImg}
-                            bookPath={item.path}
-                        />
-                    )
-                })
-            }
+            <div className="search-wrapper">
+                <div className="icon-search-wrapper">
+                    <BiIcons.BiSearch className="icon-search" />
+                </div>
+                <input
+
+                    placeholder={`Cari`}
+                />
+            </div>
+
+            <div className="books">
+                {
+                    DataBuku.map((item, index) => {
+                        return (
+                            <CardCatalog
+                                key={index}
+                                label={item.label}
+                                bookImg={item.bookImg}
+                                bookPath={item.path}
+                            />
+                        )
+                    })
+                }
+            </div>
         </BukuWrapper>
     );
 }
--- src/pages/admin/buku/Buku.style.js
+++ src/pages/admin/buku/Buku.style.js
@@ -4,9 +4,49 @@ const BukuWrapper = styled.div `
     width: fit-content;
     padding-top: 5rem;
     padding-left: 13rem;
-    grid-template-columns: auto auto auto auto auto;
-    grid-gap: 1rem;
-    display: grid;
+    
+    .books {
+        grid-template-columns: auto auto auto auto auto;
+        grid-gap: 1rem;
+        display: grid;
+        background-color: orange;
+    }
+    .search-wrapper {
+        display: flex;
+        position: relative;
+    }
+
+  .search-wrapper input{
+    background-color: #FBFBFB;
+    width: 40.125rem;
+    padding: 0.5rem;
+    padding-left: 2.5rem;
+    font-size: 1rem;
+    border-radius: 100px;
+    border: 0;
+  }
+
+  .search-wrapper input:focus{
+    box-shadow: green;
+  }
+
+  .icon-search-wrapper{
+    display: flex;
+    align-items: center;
+    height: 1.5rem;
+    width: 1.5rem;
+    padding: 0.25rem;
+    position: absolute;
+    box-sizing:border-box;
+    top:50%;
+    left:0.75rem;
+    transform: translateY(-50%);
+  }
+
+  .icon-search {
+    color: #999999;
+    font-size: 1.5rem;
+  }
 `
 
 export default BukuWrapper;
